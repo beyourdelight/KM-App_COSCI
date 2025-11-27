b18ec39b7b43ca6c58f602c28abd6a9d85b08360
beyourdelight <chutikarn.ruangp@gmail.com>
2025-11-27 14:20:31 +0700

    Update config with new Ngrok URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,4 @@
 const CONFIG = {
-    // ตอนนี้ใช้ localhost ไปก่อน เดี๋ยวพอรัน heeling-deer ค่อยมาเปลี่ยนตรงนี้
-    API_URL: "http://localhost:1337",
-    MEDIA_URL: "http://localhost:1337"
+    API_URL: "https://lashonda-knarry-tayna.ngrok-free.dev", 
+    MEDIA_URL: "https://lashonda-knarry-tayna.ngrok-free.dev"
 };
